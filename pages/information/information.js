00ce0d52df79a7f733305f7c4653fc327cc2d393
xjzsq <sdxjzsq@126.com>
2021-05-25 15:25:08 +0800

    info table

--- pages/information/information.js
+++ pages/information/information.js
@@ -5,7 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    infoType: {
+      "基本信息": {
+        "姓名": "name",
+        "学号": "stu_id",
+        "性别": "sex",
+        "班级": "class",
+        "学院": "college",
+        "专业": "major",
+        "手机1": "phone1",
+        "手机2": "phone2",
+        "身份证号": "ID",
+        "QQ号": "qq",
+        "微信号": "weixin"
+      },
+      "成绩信息": {
+        "四级成绩": "CET4",
+        "六级成绩": "CET6",
+        "计算机等级考试": "NCRE",
+        "大一绩点": "GPA_1",
+        "当前绩点": "GPA"
+      },
+      "寝室信息": {
+        "苑区": "domitory_yuan",
+        "楼栋名称": "domitory_building",
+        "寝室号": "domitory_no",
+        "小寝室号": "domitory_no2",
+        "床位号": "domitory_bed"
+      },
+      "父母或监护人信息": {
+        "父母或监护人1姓名": "parent1_name",
+        "父母或监护人1身份证件号码": "parent1_id",
+        "父母或监护人2姓名": "parent2_name",
+        "父母或监护人2身份证件号码": "parent2_id"
+      },
+      "详细信息": {
+        "入学日期": "date_to_school",
+        "生源地": "source_place",
+        "录取专业": "admission_college",
+        "录取专业": "admission_major",
+      },
+      "家庭住址": {
+        "省份": "province",
+        "市": "city",
+        "县（区）": "county",
+        "乡镇/街道": "township",
+        "社区/村": "village"
+      },
+      "其他信息": {
+
+      }
+    },
+    infoList: {
+      "name": "李魔龙",
+      "stu_id": "Q19010199",
+      "sex": "?"
+    }
   },
 
   /**
